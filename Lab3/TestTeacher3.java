45ffe4bab6f196f986d97396de49d7725668a5a5
Manasha <133505850+Asha3063@users.noreply.github.com>
2023-09-05 21:54:19 +0545

    Add files via upload

---
+++ Lab3/TestTeacher3.java
@@ -0,0 +1,10 @@
+
+package Lab3;
+
+public class TestTeacher3 {
+      public static void main(String[] args) {
+        C1 c=new C1(11,"ram","ram",1000.0,100);
+        c.showT1();
+        
+    }
+}
